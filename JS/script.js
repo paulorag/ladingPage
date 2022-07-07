ce8cb30d2ee127a1752ddf0f49d1815fa0dc01ff
Paulo Roberto <89588460+paulorag@users.noreply.github.com>
2022-07-07 08:30:42 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,2 @@
-//var today = new Date().toISOString().split('T')[0];
-//document.getElementsByName("somedate")[0].setAttribute('min',today);
+var today = new Date().toISOString().split('T')[0];
+document.getElementsByName("somedate")[0].setAttribute('min',today);
